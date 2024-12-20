6f74e724c42e4c81ce874628edb048f4f67d1c73
Tohsaka <origamikertaslipat@gmail.com>
2024-12-21 00:48:43 +0700

    refactor models userRequest

--- models/user.go
+++ models/user.go
@@ -7,16 +7,16 @@ import (
 )
 
 type User struct {
-	ID        	uint `gorm:"primarykey"`
-	CreatedAt 	time.Time
-	UpdatedAt 	time.Time
-	DeletedAt 	gorm.DeletedAt `gorm:"index"`
-	Nama      	string
-	Email     	string
-	Password  	string
-	Foto	  	string
-	Diabetes	int
-	Maag		int
-	Asam_urat	int
-	Hipertensi	int
+	ID         uint `gorm:"primarykey"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	Name       string
+	Email      string
+	Password   string
+	Foto       string
+	Diabetes   int
+	Maag       int
+	Asam_urat  int
+	Hipertensi int
 }
--- models/web/userRequest.go
+++ models/web/userRequest.go
@@ -1,12 +1,9 @@
 package web
 
-import "app/models"
-
 type UserRequest struct {
-	Name     string      `json:"name" form:"name"`
-	Email    string      `json:"email" form:"email"`
-	Password string      `json:"password" form:"password"`
-	Role     models.Role `json:"role" form:"role"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
 }
 
 type LoginRequest struct {
