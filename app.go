a2e18646e4e8d9273806362eae9ba968ebc8f18b
yusufUcps <yusuffy2212@gmail.com>
2024-12-21 17:49:36 +0700

    Update : Confiq

--- app.go
+++ app.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"masaku/config"
+	"masaku/controller"
+	"masaku/database"
+	"masaku/helpers"
 	"masaku/routes"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	e := echo.New()
+	var config = config.InitConfig()
 
-	config.ConnectDB()
+	db := database.InitModel(*config)
+	database.Migrate(db)
 
-	e := routes.Init()
+	jwtInterface := helpers.New(config.Secret)
+	// openAiInterface := helpers.NewOpenAi(config.OpenAiKey)
+	// CloudinaryInterface := helpers.NewCloudninary(config.CloudinaryKey)
 
-	e.Logger.Fatal(e.Start(":8000"))
+	userControll := controller.NewUsersControl(db, jwtInterface)
+
+
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	// Middleware CORS
+	e.Use(middleware.CORS())
+
+	// Middleware Logger
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}, time=${time_rfc3339}\n",
+	}))
+
+	// Rute untuk pengguna
+	routes.RouteUser(e, userControll, *config)
+
+	// Jalankan server
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", config.ServerPort)))
 }
--- routes/api.go
+++ routes/api.go
@@ -1,34 +1,24 @@
 package routes
 
 import (
+	"masaku/config"
 	"masaku/controller"
-	"masaku/middleware"
-	"net/http"
-	"os"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func Init() *echo.Echo {
-	AdminSecretKey := os.Getenv("ADMIN_SECRET")
-	UserSecretKey := os.Getenv("USER_SECRET")
+// RouteUser defines routes for user-related actions
+func RouteUser(e *echo.Echo, uc controller.UsersControlInterface, cfg config.ProgramConfig) {
+	// Public routes
+	e.POST("/users", uc.Register)       // Register a new user
+	e.POST("/users/login", uc.LoginUser) // User login
 
-	e := echo.New()
+	// Protected routes
+	protected := e.Group("/user", echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte(cfg.Secret),
+	}))
 
-	e.Use(middleware.NotFoundHandler)
-	Admin := e.Group("")
-	Admin.Use(echojwt.JWT([]byte(AdminSecretKey)))
-	User := e.Group("")
-	User.Use(echojwt.JWT([]byte(UserSecretKey)))
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Welcome to RESTful API Services")
-	})
-
-	//BASIC LOGIN REGISTER USER/ADMIN
-	e.POST("/register", controller.Store)
-	e.POST("/user/login", controller.LoginUser)
-
-	return e
-
+	protected.GET("/profile", uc.Profile)            // Get authenticated user's profile
+	protected.GET("/:id", uc.Show)    // Update authenticated user's profile
 }
